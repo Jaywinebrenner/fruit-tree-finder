144adbc1d80e2f33633f9fb0618bfe60ee0a500b
jay winebrenner <jaywinebrenner@jays-iMac-2.local>
2020-07-15 14:45:07 -0700

    add expand and collapse but still wonky

--- screens/DrawerHomeSwipe.js
+++ screens/DrawerHomeSwipe.js
@@ -10,7 +10,9 @@ import {
   Image,
   ScrollView,
   TouchableOpacity,
-  Dimensions
+  Dimensions,
+  UIManager,
+  LayoutAnimation,
 } from "react-native";
 import { Foundation } from "@expo/vector-icons";
 import BottomSheet from "reanimated-bottom-sheet";
@@ -26,13 +28,27 @@ import customTreeBox from "../media/customTreeBox.png";
 const DrawerHomeSwipe = (props) => {
 
   const windowHeight = Dimensions.get('window').height;
-  const drawerTopHeight = (windowHeight * .84);
-  const drawerHalfHeight = (windowHeight * .5);
-  const drawerBottomHeight = (windowHeight * .04);
+  const drawerTopHeight = (windowHeight * .52);
+  const drawerHalfHeight = (windowHeight * .18);
+  const drawerBottomHeight = (windowHeight * .08);
+ // ANDROID const drawerBottomHeight = (windowHeight * .04)
 
   const userCoords = props.userCoords;
   const treeList = props.treeList;
   const filter = props.filter;
+
+  const [expanded, setExpanded] = useState(false)
+
+
+  if (Platform.OS === 'android') {
+    UIManager.setLayoutAnimationEnabledExperimental(true);
+  }
+  
+  const dropDown = () => {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+    setExpanded(!expanded)
+  };
+
 
   const [treeArray, setTreeArray] = useState(null);
 
@@ -65,15 +81,28 @@ const DrawerHomeSwipe = (props) => {
     }
   }
 
+  const renderDeleteButton = treeList && (
+    <View onPress={() => areYouSure()}>
+      <AntDesign
+        style={styles.deleteIcon}
+        name="delete"
+        size={30}
+        color="white"
+        onPress={() => areYouSure()}
+      />
+    </View>
+  );
+
+
   const renderContent = () => {
 
     return (
       <React.Fragment>
-        <View style={styles.topStrip}>
-          <View style={styles.dragView} />
-        </View>
+        <ImageBackground source={maroonGradient} style={styles.gradientImage}>
+          <View style={styles.topStrip}>
+            <View style={styles.dragView} />
+          </View>
           <ScrollView>
-          <ImageBackground source={maroonGradient} style={styles.gradientImage}>
           <Entypo
           name="tree"
           size={500}
@@ -84,33 +113,59 @@ const DrawerHomeSwipe = (props) => {
               treeArray.map((value, index) => {
                 return (
                   <TouchableOpacity>
-                  <View style={styles.cardContainer} key={index}>
-                    <View style={{ flexDirection: "row" }}>
-                      <Image style={styles.boxTree} source={customTreeBox} />
-                      <View style={styles.cardInfo}>
-                        <Text style={styles.cardTitleText}>{value.type}</Text>
-                        <Text style={styles.cardDistanceText}>
-                          {milesOrYards(value.distance)}
+                    <View style={styles.cardContainer} key={index}>
+                      <View style={{ flexDirection: "row" }}>
+                        <Image style={styles.boxTree} source={customTreeBox} />
+                        <View style={styles.cardInfo}>
+                          <Text style={styles.cardTitleText}>{value.type}</Text>
+                          <Text style={styles.cardDistanceText}>
+                            {milesOrYards(value.distance)}
+                          </Text>
+                        </View>
+                        <TouchableOpacity
+                          style={styles.cardDetailsButtonWrapper}
+                          // onPress={() =>
+                          //   navigation.navigate("ListItemDetailScreen", {
+                          //     index,
+                          //     ...value,
+                          //   })
+                          // }
+                          onPress={() => dropDown()}
+                        >
+                          <Text style={styles.cardDetailsButtonText}>
+                            Details
+                          </Text>
+                        </TouchableOpacity>
+                      </View>
+                    <View
+                      style={{
+                        height: expanded ? 220 : 0,
+                        overflow: "hidden",
+                        paddingLeft: 10,
+                        paddingRIght: 10,
+                      }}
+                    >
+                      <View style={styles.locationWrapper}>
+                        <Text style={styles.treeLocationText}>
+                          {value.treeLocation}
                         </Text>
                       </View>
-                      <TouchableOpacity
-                        style={styles.cardDetailsButtonWrapper}
-                        onPress={() =>
-                          navigation.navigate("ListItemDetailScreen", {
-                            index,
-                            ...value,
-                          })
-                        }
-                      >
-                        <Text style={styles.cardDetailsButtonText}>Details</Text>
-                      </TouchableOpacity>
+                      <View style={styles.descriptionWrapper}>
+                        <Text style={styles.descriptionText}>
+                          {value.description}
+                        </Text>
+                      </View>
+                      <View style={styles.deleteButtonWrapper}>
+                      { renderDeleteButton }
+                      </View>
                     </View>
-                  </View>
+                    </View>
+
                   </TouchableOpacity>
                 );
               })}
-              </ImageBackground>
           </ScrollView>
+        </ImageBackground>
       </React.Fragment>
     );
   };
@@ -125,81 +180,100 @@ const DrawerHomeSwipe = (props) => {
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-    },
-    top: {
-      paddingTop: 25,
-      height: 80,
-      backgroundColor: "rgba(236, 250, 217, .2)",
-    },
-    cardContainer: {
-      justifyContent: "center",
-      width: "100%",
-      marginTop: 12,
-      padding: 10,
-      color: "red",
-      backgroundColor: "rgba(255, 255, 255, .05)",
-    },
-    cardDetailsButtonWrapper: {
-      position: "absolute",
-      right: "3%",
-      borderWidth: 1,
-      borderRadius: 2,
-      padding: 4,
-      borderColor: "white",
-      ...Platform.select({
-        ios: {
-          bottom: 17,
-        },
-      }),
-    },
-    cardDetailsButtonText: {
-      color: "white",
-    },
-    cardDistanceText: {
-      fontSize: 15,
-      color: "white",
-    },
-    cardTitleText: {
-      fontSize: 22,
-      color: "white",
-    },
-    cardDescriptionText: {
-      color: "rgba(255, 255, 255, .5)",
-    },
-    gradientImage: {
-      height: "100%",
-      width: "100%",
-    },
-    bigTree: {
-      position: "absolute",
-      bottom: -50,
-      top: 300,
-      right: -60,
-    },
-    boxTree: {
-      marginRight: "3%",
-      width: 40,
-      height: 40
-    },
-    topStrip: {
-      height: 30,
-      backgroundColor: "rgba(255, 255, 255, .7)",
-      // transparent maroon?
-      // backgroundColor: "rgba(83, 22, 19, .5)",
-      justifyContent: "center",
-      alignItems: "center",
-    },
-    dragView: {
-      paddingTop: 0,
-      borderBottomColor: "#531613",
-      borderBottomWidth: 8,
-      width: 50,
-      borderRadius: 9,
-      padding: 0,
-      margin: 0,
-    }
-  });
+  container: {
+    flex: 1,
+  },
+  top: {
+    paddingTop: 25,
+    height: 80,
+    backgroundColor: "rgba(236, 250, 217, .2)",
+  },
+  cardContainer: {
+    justifyContent: "center",
+    width: "100%",
+    marginTop: 12,
+    padding: 10,
+    color: "red",
+    backgroundColor: "rgba(255, 255, 255, .05)",
+  },
+  cardDetailsButtonWrapper: {
+    position: "absolute",
+    right: "3%",
+    borderWidth: 1,
+    borderRadius: 2,
+    padding: 4,
+    borderColor: "white",
+    ...Platform.select({
+      ios: {
+        bottom: 17,
+      },
+    }),
+  },
+  cardDetailsButtonText: {
+    color: "white",
+  },
+  cardDistanceText: {
+    fontSize: 15,
+    color: "white",
+  },
+  cardTitleText: {
+    fontSize: 22,
+    color: "white",
+  },
+  cardDescriptionText: {
+    color: "rgba(255, 255, 255, .5)",
+  },
+  gradientImage: {
+    height: "100%",
+    width: "100%",
+  },
+  bigTree: {
+    position: "absolute",
+    bottom: -50,
+    top: 300,
+    right: -60,
+  },
+  boxTree: {
+    marginRight: "3%",
+    width: 40,
+    height: 40,
+  },
+  topStrip: {
+    height: 60,
+    backgroundColor: "rgba(255, 255, 255, .7)",
+    // transparent maroon?
+    // backgroundColor: "rgba(83, 22, 19, .5)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  dragView: {
+    paddingTop: 0,
+    borderBottomColor: "#531613",
+    borderBottomWidth: 8,
+    width: 50,
+    borderRadius: 9,
+    padding: 0,
+    margin: 0,
+  },
+  descriptionWrapper: {
+    marginTop: 15,
+    height: 130
+  },
+  descriptionText: {
+    fontSize: 20,
+    color: "white",
+  },
+  locationWrapper: {
+    marginTop: 10
+  },
+  treeLocationText: {
+    fontSize: 20,
+    color: "white",
+  },
+  deleteButtonWrapper: {
+
+    marginLeft: 10
+  },
+});
 
 export default DrawerHomeSwipe;
